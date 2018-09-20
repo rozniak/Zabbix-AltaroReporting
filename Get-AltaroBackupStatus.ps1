565678a48ddcd125abe39268da0b7698817337b1
Rory Fewell <roryfewell@hotmail.com>
2018-09-20 09:47:32 +0100

    Prelim: Write up script that will report the state of Altaro backups and another for installing the trap into Task Scheduler

---
+++ Get-AltaroBackupStatus.ps1
@@ -0,0 +1,86 @@
+﻿<#
+    .SYNOPSIS
+    This script is used for obtaining the status of backups that have taken place in
+    Altaro VM Backup.
+
+    .DESCRIPTION
+    This script utilises the logs that Altaro VM Backup outputs into Event Viewer in
+    order to determine the condition of onsite and offsite backup jobs.
+
+    .PARAMETER BackupType
+    The type of backup to check. (Choices: Offsite,
+                                           Onsite)
+
+    .PARAMETER ZabbixIP
+    The IP address of the Zabbix server/proxy to send the value to.
+    
+    .EXAMPLE
+    Get-AltaroBackupStatus.ps1 -BackupType Offsite -ZabbixIP 10.0.0.240
+
+    .NOTES
+    Author: Rory Fewell
+    GitHub: https://github.com/rozniak
+    Website: https://oddmatics.uk
+#>
+
+Param (
+    [Parameter(Position=0, Mandatory=$TRUE)]
+    [String]
+    $BackupType,
+    [Parameter(Position=1, Mandatory=$TRUE)]
+    [ValidatePattern("^(\d+\.){3}\d+$")]
+    [String]
+    $ZabbixIP
+)
+
+$altaroEvents = Get-EventLog -Source "Altaro VM Backup" -LogName Application -Newest 10;
+$filterBackupType = "";
+
+switch ($BackupType.ToLower())
+{
+    "offsite" {
+        $filterBackupType = "Offsite Copy Result";
+    }
+
+    "onsite" {
+        $filterBackupType = "Backup Result";
+    }
+
+    default {
+        throw [System.ArgumentException] "Unknown backup type specified.";
+    }
+}
+
+# Search for the backup type we want
+#
+for ($i = 0; $i -lt $altaroEvents.Length; $i++)
+{
+    $entryType = $altaroEvents[$i].EntryType;
+    $message   = $altaroEvents[$i].Message.Split([Environment]::NewLine)[2];
+
+    # If this isn't the right type, skip this iteration
+    #
+    if (-Not ($message.StartsWith($filterBackupType)))
+    {
+        continue;
+    }
+
+    # This is the result we want, analyse it
+    #
+    switch ($entryType)
+    {
+        "Error" {
+            return 1; # Failure
+        }
+
+        "Information" {
+            return 0; # Success
+        }
+
+        default {
+            return 2; # Unknown
+        }
+    }
+}
+
+return 2; # Unknown - we didn't find anything!!
---
+++ Install-AltaroZabbixTraps.ps1
@@ -0,0 +1,45 @@
+﻿<#
+    .SYNOPSIS
+    This script is used for installing the PowerShell scripts used for providing Zabbix
+    traps into the task scheduler on the local machine.
+
+    .DESCRIPTION
+    This script first queries for the IP of the Zabbix Server or Proxy that is then
+    embedded as part of the parameters used in the scheduled task actions. The tasks are
+    scheduled to run hourly into order to provide data to Zabbix via traps.
+    
+    .EXAMPLE
+    Install-AltaroZabbixTraps.ps1
+
+    .NOTES
+    Author: Rory Fewell
+    GitHub: https://github.com/rozniak
+    Website: https://oddmatics.uk
+#>
+
+Param (
+    [Parameter(Position=0, Mandatory=$TRUE)]
+    [ValidatePattern("^(\d+\.){3}\d+$")]
+    [String]
+    $ZabbixIP
+)
+
+$globalTrigger = New-ScheduledTaskTrigger -Daily -At 8am
+$systemPrincipal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+# Set up Altaro VM Backup status tasks
+#
+$backupTypes = (
+    "Offsite",
+    "Onsite"
+)
+
+foreach ($backupType in $backupTypes)
+{
+    $monitorAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument ('-NoProfile -NoLogo -File "' + $env:ProgramFiles + '\Zabbix Agent\ALTAROREPORTS\Get-AltaroBackupStatus.ps1" -BackupType ' + $backupType + ' -ZabbixIP ' + $ZabbixIP)
+
+    $monitorTask = Register-ScheduledTask -TaskName ("Report Altaro VM Backup State for " + $backupType  + " backups (Zabbix Trap)") -Trigger $globalTrigger -Action $monitorAction -Principal $systemPrincipal
+
+    $monitorTask.Triggers[0].Repetition.Interval = "PT1H"
+    $monitorTask | Set-ScheduledTask
+}
