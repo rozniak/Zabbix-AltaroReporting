25098f54bb28c0a2b26fe819d362628e2a417080
Rory Fewell <roryfewell@hotmail.com>
2018-11-01 15:50:22 +0000

    Enhancement: Make onsite success a prerequisite for offsites, and also check the last backup was within a theshold of 2 days

--- Get-AltaroBackupStatus.ps1
+++ Get-AltaroBackupStatus.ps1
@@ -26,25 +26,48 @@
 Param (
     [Parameter(Position=0, Mandatory=$TRUE)]
     [String]
-    $BackupType,
-    [Parameter(Position=1, Mandatory=$TRUE)]
-    [ValidatePattern("^(\d+\.){3}\d+$")]
-    [String]
-    $ZabbixIP
+    $BackupType
 )
 
+# FUNCTION DEFINITIONS
+#
+Function Send-ZabbixValue
+{
+    Param (
+        [Parameter(Position=0, Mandatory=$TRUE)]
+        [System.Byte]
+        $ResultCode
+    )
+
+    Write-Host $ResultCode;
+
+    Exit
+
+    # Push value to Zabbix
+    #
+    & ($env:ProgramFiles + "\Zabbix Agent\bin\win64\zabbix_sender.exe") ("-z", $ZabbixIP, "-p", "10051", "-s", $env:ComputerName, "-k", ("altaro.backupstatus[" + $BackupType.ToLower() + "]"), "-o", $ResultCode)
+}
+
+# CONSTANTS
+#
+$cutoffDateTime = [System.DateTime]::UtcNow.AddDays(-2);
+$keywordOffsite = "Offsite Copy Result";
+$keywordOnsite = "Backup Result";
+
+# MAIN SCRIPT STARTS
+#
 $altaroEvents = Get-EventLog -Source "Altaro VM Backup" -LogName Application -Newest 10;
-$filterBackupType = "";
-$resultCode = 2;
+$checkOffsite = $FALSE;
+$resultCode = 3; # Unknown
 
 switch ($BackupType.ToLower())
 {
     "offsite" {
-        $filterBackupType = "Offsite Copy Result";
+        $checkOffsite = $TRUE;
     }
 
     "onsite" {
-        $filterBackupType = "Backup Result";
+        $checkOffsite = $FALSE;
     }
 
     default {
@@ -52,38 +75,91 @@ switch ($BackupType.ToLower())
     }
 }
 
-# Search for the backup type we want
+# Search for first instance of onsite and offsite backup reports
 #
+$eventOffsite = $NULL;
+$eventOnsite  = $NULL;
+$foundOffsite = $FALSE;
+$foundOnsite  = $FALSE;
+
 for ($i = 0; $i -lt $altaroEvents.Length; $i++)
 {
-    $entryType = $altaroEvents[$i].EntryType;
     $message   = $altaroEvents[$i].Message.Split([Environment]::NewLine)[2];
 
-    # If this isn't the right type, skip this iteration
+    # Check for offsite
     #
-    if (-Not ($message.StartsWith($filterBackupType)))
+    if (-Not $foundOffsite -And $message.StartsWith($keywordOffsite))
     {
+        $eventOffsite = $altaroEvents[$i];
+        $foundOffsite = $TRUE;
+
         continue;
     }
 
-    # This is the result we want, analyse it
+    # Check for onsite
     #
-    switch ($entryType)
+    if (-Not $foundOnsite -And $message.StartsWith($keywordOnsite))
     {
-        "Error" {
-            $resultCode = 1; # Failure
-        }
+        $eventOnsite = $altaroEvents[$i];
+        $foundOnsite = $TRUE;
 
-        "Information" {
-            $resultCode = 0; # Success
-        }
-
-        default {
-            $resultCode = 2; # Unknown
-        }
+        continue;
     }
 }
 
-# Push value to Zabbix
+# Always check onsite first
 #
-& ($env:ProgramFiles + "\Zabbix Agent\bin\win64\zabbix_sender.exe") ("-z", $ZabbixIP, "-p", "10051", "-s", $env:ComputerName, "-k", ("altaro.backupstatus[" + $BackupType.ToLower() + "]"), "-o", $resultCode)
+switch ($eventOnsite.EntryType)
+{
+    "Error" {
+        Send-ZabbixValue -ResultCode 1 # Failure
+    }
+
+    "Information" {
+        $resultCode = 0; # Success
+    }
+    
+    default {
+        Send-ZabbixValue -ResultCode 3; # Unknown
+    }
+}
+
+# Ensure onsite was within the threshold
+#
+if ($eventOnsite.TimeGenerated -le $cutoffDateTime)
+{
+    Send-ZabbixValue -ResultCode 2; # Out of date
+}
+
+# See if we need to check offsite
+#
+if (-Not $checkOffsite)
+{
+    Send-ZabbixValue -ResultCode $resultCode;
+}
+
+switch ($eventOffsite.EntryType)
+{
+    "Error" {
+        Send-ZabbixValue -ResultCode 1; # Failure
+    }
+
+    "Information" {
+        $resultCode = 0; # Success
+    }
+
+    default {
+        Send-ZabbixValue -ResultCode 3; # Unknown
+    }
+}
+
+# Ensure offiste was within the threshold
+#
+if ($eventOffsite.TimeGenerated -le $cutoffDateTime)
+{
+    Send-ZabbixValue -ResultCode 2; # Out of date
+}
+else
+{
+    Send-ZabbixValue -ResultCode 0; # Success
+}
